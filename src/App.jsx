59cd7a9ea66b8f0feac85944b8524109bb8969be
Guruprakash <ticvictech@Ticvics-MacBook-Pro.local>
2021-11-17 14:53:27 +0530

    Changes in root file App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 
 import "./index.scss";
 
+const currentData = new Date();
+
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: react-mf</div>
+    <div>Name: React New Build | {currentData}</div>
     <div>Framework: react</div>
     <div>Language: JavaScript</div>
     <div>CSS: Tailwind</div>
